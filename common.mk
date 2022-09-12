278b008cb75f451e2b4d0202a35ece4101877d5d
Mathieu Chartier <mathieuc@google.com>
2022-09-11 23:17:20 -0300

    sdm660-common: Always preopt extracted APKs

--- common.mk
+++ common.mk
@@ -109,6 +109,7 @@ $(call inherit-product, frameworks/native/build/phone-xhdpi-4096-dalvik-heap.mk)
 
 # Speed profile services and wifi-service to reduce RAM and storage
 PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
+PRODUCT_ALWAYS_PREOPT_EXTRACTED_APK := true
 
 # Display
 PRODUCT_PACKAGES += \
